5dd3dc6536970d6be7149f6148cf661431b15a2b
PSK1738 <philip.kalachev@gmail.com>
2025-08-26 23:47:15 +0100

    New files added

---
+++ problem_007.py
@@ -0,0 +1,17 @@
+import math
+from math import sqrt
+primes = []
+
+for number in range(2,150000):
+    prime = True
+    for x in range(2, int(math.isqrt(number) + 1)):
+        if number % x == 0:
+            prime = False
+            break
+    if prime:
+        primes.append(number)
+        if len(primes) > 10001:
+            break
+
+
+print(primes[10000])
